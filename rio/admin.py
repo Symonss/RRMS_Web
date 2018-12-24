bfc36beeb2279c0beedc73397615bd98668580f5
Symons <somborisymonss@gmail.com>
2018-12-25 00:07:45 +0300

    models half way done

--- rio/admin.py
+++ rio/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Oder
 from .models import User
 from .models import Item
+from .models import Message
 # Register your models here.
 admin.site.register(Oder)
 admin.site.register(User)
 admin.site.register(Item)
+admin.site.register(Message)
--- rio/models.py
+++ rio/models.py
@@ -10,12 +10,22 @@ class User (models.Model):
     def __str__(self):
         return self.user_name
 
+class Item(models.Model):
+    name = models.CharField(max_length = 50)
+    price = models.IntegerField()
+    pic_link = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.name
+
+
 class Oder(models.Model):
     quontity = models.IntegerField()
     note = models.TextField()
     created_time = models.DateTimeField(default=timezone.now)
     deliverd_time = models.DateTimeField(blank=True , null=True)
     is_delivered = models.BooleanField(default=False)
+    items = models.ForeignKey(Item, on_delete=models.CASCADE , related_name = 'order_items')
     users = models.ForeignKey(User,related_name='username', on_delete=models.CASCADE)
 
     def delivered(self):
@@ -26,17 +36,17 @@ class Oder(models.Model):
         return self.note
 
     def delivered_orders(self):
-        return self.Orders.filter(is_delivered=True)
+        return self.Order.filter(is_delivered=True)
 
+class Message(models.Model):
+    name = models.CharField(max_length = 35)
+    email = models.CharField(max_length=35)
+    newsletter = models.BooleanField(default=False)
+    text = models.TextField()
+    is_read= models.BooleanField(default=False)
 
-
-class Item(models.Model):
-    name = models.CharField(max_length = 50)
-    oders = models.ForeignKey(Oder, on_delete=models.CASCADE , related_name = 'order_items')
-    price = models.IntegerField()
-    pic_link = models.CharField(max_length=70)
-
-    def __str__(self):
-        return self.name
+    def read(self):
+        self.is_read = true
+        self.save()
 
 # oders object
